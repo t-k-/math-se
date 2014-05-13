8ba9c994b5137fbfb3bad8b9e1211ba65686f314
t-k- <clock126@126.com>
2014-05-13 18:10:19 -0400

    cat function for our own

--- httpd/httpd.c
+++ httpd/httpd.c
@@ -3,6 +3,21 @@
 #include <stdlib.h>
 #include "../inter-def.h"
 
+void cat(char *file)
+{
+	size_t len = 0;
+	char *line = NULL;
+	FILE *f = fopen(file, "r");
+	if (f == NULL)
+		return;
+
+	while (getline(&line, &len, f) != -1)
+		printf("%s", line);
+
+	free(line);
+	fclose(f);
+}
+
 void replace_plus(char *str)
 {
 	while (str[0] != '\0') {
@@ -21,28 +36,35 @@ int main()
 	char arg[2048];
 	char cmd[2048];
 	char line[4096];
+	int start;
 	FILE *f_rank = NULL;
 	printf("Content-type: text/html\n\n");
 
 	scanf("%s", arg);
 	get_str = getenv("QUERY_STRING");
+	sscanf(get_str, "s=%d", &start);
 	
-	printf("get: %s </br>", get_str);
-	printf("post: %s </br>", arg);
+	if (start == 0) {
+		replace_plus(arg);
+		curl = curl_easy_init();
+		unescape = curl_easy_unescape(curl, arg, 0, NULL);
 
-	replace_plus(arg);
-	curl = curl_easy_init();
-	unescape = curl_easy_unescape(curl, arg, 0, NULL);
+		sprintf(arg, "%s", unescape);
+		unescape[0] = ' ';
+		unescape[1] = ' ';
 
-	sprintf(arg, "%s", unescape);
-	arg[0] = ' ';
-	arg[1] = ' ';
-	free(unescape);
+		sprintf(cmd, "./ma-se '%s'", unescape);
+		system(cmd);
 
-	sprintf(cmd, "./ma-se '%s'", arg);
-	system(cmd);
-	printf("<b>for query: %s </b><br/>", unescape);
+		printf("<h3>results of query %s (%d to %d):</h3>", 
+				unescape, start, start + 10);
+		free(unescape);
+	} else {
+		printf("<h3>results (%d to %d):</h3>", 
+				start, start + 10);
+	}
 
+	cat("dog");
 	printf("rank: </br>");
 	f_rank = fopen("rank", "r");
 	if (f_rank == NULL) {
@@ -58,5 +80,6 @@ int main()
 			"</html>");
 	
 	fclose(f_rank);
+
 	return 0;
 }
