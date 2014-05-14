d38ff1bae2aa3c429556e763a4aa8707460b0a55
t-k- <clock126@126.com>
2014-05-14 03:29:30 -0400

    my WEB interface looks nice!

--- httpd/httpd.c
+++ httpd/httpd.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../inter-def.h"
 
 void cat(char *file)
@@ -35,7 +36,9 @@ int main()
 	char *get_str;
 	char arg[2048];
 	char cmd[2048];
-	char line[4096];
+	char *line;
+	char last_line[4096];
+	size_t len = 0;
 	int toggle = 0;
 	int start;
 	FILE *f_rank = NULL;
@@ -43,8 +46,14 @@ int main()
 
 	scanf("%s", arg);
 	get_str = getenv("QUERY_STRING");
+
+	if (get_str == NULL) {
+		printf("no GET arg.\n");
+		return 0;
+	}
+
 	sscanf(get_str, "s=%d", &start);
-	
+
 	if (start == 0) {
 		replace_plus(arg);
 		curl = curl_easy_init();
@@ -57,25 +66,39 @@ int main()
 		sprintf(cmd, "./ma-se '%s'", unescape);
 		system(cmd);
 
-		printf("<h3>results of query %s (%d to %d):</h3>", 
-				unescape, start, start + 10);
+		/* printf("<h3>results of query %s (%d to %d):</h3>", 
+				unescape, start, start + 10); */
+		strcpy(arg, unescape);
 		free(unescape);
 	} else {
-		printf("<h3>results (%d to %d):</h3>", 
-				start, start + 10);
+		/* printf("<h3>results (%d to %d):</h3>", 
+				start, start + 10); */
 	}
 
 	cat("head.cat");
+	printf("[imath] \\quad %s [/imath] : </p><hr class=\"rgsep\">"
+			"<ul style=\"list-style-type: none;\">", arg);
+
 	f_rank = fopen("rank", "r");
 	if (f_rank == NULL) {
 		printf("no thing we can find :( <br/>");
 		return 0;
 	}
 
-	while (fscanf(f_rank, "%s\n", line) != EOF) {
-		printf("[%s]</br>", line);
+	while (getline(&line, &len, f_rank) != -1) {
+		if (toggle == 0) {
+			strcpy(last_line, line);
+		} else {
+			printf("<li><table border=\"0\"><tr><td><a href=%s"
+					"><h3>%s</h3></a><span style=\"color:green;\">"
+					"%s</span></td><td>[dmath]%s"
+					"[/dmath]</td></tr></table></li>",
+					line, last_line, line, last_line);
+		}
+
 		toggle = (toggle + 1) % 2;
 	}
+	free(line);
 	
 	fclose(f_rank);
 	cat("tail.cat");
