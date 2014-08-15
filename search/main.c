5fb32adc07f163a36c1bedc7677e14d0b1001e7e
t-k- <clock126@126.com>
2014-08-15 12:06:15 -0400

    li_brw_name_sort() added and tested.

--- search/main.c
+++ search/main.c
@@ -3,11 +3,12 @@
 #include "mathtree.h"
 
 static
-LIST_IT_CALLBK(test)
+LIST_IT_CALLBK(print)
 {
 	int i;
 	LIST_OBJ(struct query_brw, p, ln);
 	printf("%s ", p->dir);
+	printf("[%d] ", p->same_name_cnt);
 	printf("%s ", p->vname);
 	for (i = 0;; i++) {
 		if (p->weight[i] == 0)
@@ -16,6 +17,57 @@ LIST_IT_CALLBK(test)
 	}
 	printf("\n");
 	LIST_GO_OVER;
+}
+
+struct _get_name_cnt_arg {
+	uint  cnt;
+	char* vname;
+};
+
+static
+LIST_IT_CALLBK(_get_name_cnt)
+{
+	LIST_OBJ(struct query_brw, p, ln);
+	P_CAST(gnca, struct _get_name_cnt_arg, pa_extra);
+
+	if (strcmp(gnca->vname, p->vname) == 0)
+		gnca->cnt += p->weight[0];
+
+	LIST_GO_OVER;
+}
+
+static
+LIST_IT_CALLBK(_set_name_cnt)
+{
+	struct list_it li;
+	struct _get_name_cnt_arg gnca;
+	LIST_OBJ(struct query_brw, p, ln);
+
+	li = list_get_it(pa_head->now);
+	gnca.cnt = 0;
+	gnca.vname = (char*)p->vname;
+	list_foreach(&li, &_get_name_cnt, &gnca);
+	p->same_name_cnt = gnca.cnt;
+
+	LIST_GO_OVER;
+}
+
+static
+LIST_CMP_CALLBK(_same_name_cmp)
+{
+	struct query_brw *p0 = MEMBER_2_STRUCT(pa_node0, 
+	                                struct query_brw, ln);
+	struct query_brw *p1 = MEMBER_2_STRUCT(pa_node1, 
+	                                struct query_brw, ln);
+	return p0->same_name_cnt > p1->same_name_cnt;
+}
+
+void li_brw_name_sort(struct list_it *li)
+{
+	struct list_sort_arg lsa;
+	list_foreach(li, &_set_name_cnt, NULL);
+	lsa.cmp = &_same_name_cmp;
+	list_sort(li, &lsa);
 }
 
 int main(int argc, char *argv[])
@@ -29,8 +81,8 @@ int main(int argc, char *argv[])
 
 	printf("%s\n", query);
 	li_query_brw = tex2brwords(query);
-
-	list_foreach(&li_query_brw, &test, NULL);
+	li_brw_name_sort(&li_query_brw);
+	list_foreach(&li_query_brw, &print, NULL);
 	li_brw_release(&li_query_brw);
 
 	return 0;
