7a79629c5dd101332ade82092e736d13329b65c9
t-k- <clock126@126.com>
2014-08-26 22:45:18 -0400

    search: program arg handle order changed.

--- search/main.c
+++ search/main.c
@@ -443,34 +443,38 @@ int main(int argc, char *argv[])
 
 	if (argc != 2) {
 		printf("invalid argument format.\n");
-		return 1;
-	}
-
-	bdb_doc = bdb_init("./collection/documents.bdb");
-	if (NULL == bdb_doc) {
-		printf("tcbdb doc open error.\n");
-		return 2;
-	}
-	
-	bdb_num = bdb_init("./collection/brw-number.bdb");
-	if (NULL == bdb_num) {
-		printf("tcbdb num open error.\n");
-		return 3;
+		return __LINE__;
 	}
 
 	if (redis_cli_init("127.0.0.1", DEFAULT_REDIS_PORT)) {
 		printf("redis server is down.\n");
-		return 4;
+		return __LINE__;
 	}
 
 	if (0 == strcmp(query, "#flush")) {
 		printf("flush redis DB...\n");
 		redis_cli_free();
 		return 0;
+
 	} else if (0 == strcmp(query, "#shutdown")) {
 		printf("shutdown redis DB...\n");
 		redis_shutdown();
 		return 0;
+	}
+
+	bdb_doc = bdb_init("./collection/documents.bdb");
+	if (NULL == bdb_doc) {
+		printf("tcbdb doc open error.\n");
+		redis_cli_free();
+		return __LINE__;
+	}
+	
+	bdb_num = bdb_init("./collection/brw-number.bdb");
+	if (NULL == bdb_num) {
+		printf("tcbdb num open error.\n");
+		redis_cli_free();
+		bdb_release(bdb_doc);
+		return __LINE__;
 	}
 
 	printf("query: %s\n", query);
