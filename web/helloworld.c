ecbce7b1bb5ab4a59f1625d1d2d6b5bce4489f4c
t-k- <clock126@126.com>
2014-09-05 16:05:08 -0400

    :cake: web interface: adding cgi permission in Makefile.

---
+++ web/helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Content-type: text/html\n\n");
+	printf("hello world! <br/>");
+	printf("<img src=\"/math-se/mathman.jpg\"/>");
+	printf("\n");
+	return 0;
+}
