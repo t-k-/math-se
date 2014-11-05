800f5fe4675c8489baa10b65497a0548c190e8b8
t-k- <clock126@126.com>
2014-11-05 02:33:47 -0500

    real postmerge.c

--- search/lab/posmerge.c
+++ search/lab/posmerge.c
@@ -12,37 +12,26 @@ struct brw_blk {
 	unsigned char var;
 };
 
-unsigned int write(const char *fname)
+void write(const char *fname)
 {
 	FILE *fh = fopen(fname, "a");
 	struct brw_blk blk;
 	int i, delta;
-	unsigned int check_sum = 0;
 	
 	if (fh == NULL)
 		return;
 
 	blk.frml_id = 0;
-	for (i = 0; i < 25; i++)
-		blk.factor[i] = 25 + i;
-	blk.idx = 0;
-	blk.num = 0;
-	blk.var = 0;
-	delta = rand() % 300;
-	for (i = 0; i < 15000 + delta; i++) {
+	delta = rand() % 10;
+	for (i = 0; i < 30 + delta; i++) {
 		blk.frml_id += rand() % 5;
-		blk.idx ++;
-		blk.num = (blk.num + 1) % 2;
-		blk.var = blk.num + blk.idx;
 		fwrite(&blk, sizeof(struct brw_blk), 1, fh);
-		check_sum += blk.frml_id;
 	}
 	
 	fclose(fh);
-	return check_sum;
 }
 
-#define BRWBLK_RD_NUM 4096 // 8
+#define BRWBLK_RD_NUM 8 // 4096
 #define BRWBLK_UNIT_SZ sizeof(struct brw_blk)
 #define BRWBLK_RD_BUF_SZ (BRWBLK_RD_NUM * BRWBLK_UNIT_SZ) 
 
@@ -56,85 +45,133 @@ int read(const char *fname)
 	if (fh == NULL)
 		return;
 	
+	printf("%s:", fname);
 	do {
 		nread = fread(buff, BRWBLK_UNIT_SZ, BRWBLK_RD_NUM, fh);
 		if (nread <= 0)
 			break;
-		printf("%s: size read = %d\n", fname, nread);
+		printf("\n(size read = %d) ", nread);
 		for (j = 0; j< nread; j++) {
 			blk = (struct brw_blk*)buff + j;
-			printf("%s: %d\n", fname, blk->frml_id);
+			printf("%d ", blk->frml_id);
 			res += blk->frml_id;
 		}
 	} while (1);
 	
+	printf("\n\n");
 	fclose(fh);
 	return res;
 }
 
-unsigned int read2(char (*fname)[32], int n)
+static void 
+rebuf(void *buf, FILE *fh,
+      unsigned int *idx, unsigned int *end)
 {
-	int i;
+	unsigned int nread;
+	nread = fread(buf, BRWBLK_UNIT_SZ, BRWBLK_RD_NUM, fh);
+	*idx = 0;
+	*end = nread;
+	printf("rebuf: read %d\n", nread);
+}
+
+static void 
+jump(unsigned int target_id, struct brw_blk *buf, FILE *fh,
+     unsigned int *idx, unsigned int *end)
+{
+	unsigned int nread, i, idx_far;
+	do {
+		idx_far = *end - 1;
+		if (buf[idx_far].frml_id >= target_id)
+			break;
+		rebuf(buf, fh, idx, end);
+	} while (*idx < *end);
+
+	if (*idx == *end)
+		return;
+
+	for (i = *idx; i < *end; i++) {
+		if (buf[i].frml_id >= target_id)
+			break;
+	}
+
+	*idx = i; 
+	printf("jump to (%d)\n", buf[i].frml_id);
+}
+
+unsigned int posmerge(char (*fname)[32], int n)
+{
+	int i, term_cond;
 	FILE **fh = calloc(n, sizeof(FILE*));
 	struct brw_blk (*buf)[BRWBLK_RD_NUM];
-	unsigned int *idx, *end, nread, min;
-	unsigned int check_sum = 0;
+	unsigned int *idx, *end, max, id, diff;
 
-	/* initialization */
 	for (i = 0; i < n; i++) {
 		fh[i] = fopen(fname[i], "r");
-		if (fh[i] == NULL) 
+		if (fh[i] == NULL) {
+			printf("open error: %s\n", fname[i]);
 			goto exit;
+		}
 	}
 	
 	buf = malloc(n * BRWBLK_RD_BUF_SZ);
 	idx = malloc(n * sizeof(unsigned int));
 	end = malloc(n * sizeof(unsigned int));
 
-	for (i = 0; i < n; i++) {
-		idx[i] = BRWBLK_RD_NUM;
-		end[i] = BRWBLK_RD_NUM + 1;
-	}
-	
+	if (buf == NULL || idx == NULL || end == NULL)
+		goto exit;
+
+	for (i = 0; i < n; i++)
+		rebuf(&buf[i], fh[i], &idx[i], &end[i]);
+
 	do { 
-		/* update buffer when needed */
-		for (i = 0; i < n; i++) {
-			if (idx[i] == end[i]) {
-				continue;
-			} else if (idx[i] == BRWBLK_RD_NUM) {
-				nread = fread(buf[i], BRWBLK_UNIT_SZ, 
-				              BRWBLK_RD_NUM, fh[i]);
-
-				//printf("...........read %d\n", nread);
-				idx[i] = 0;
-				if (nread < BRWBLK_RD_NUM)
-					end[i] = nread;
+		max = buf[0][idx[0]].frml_id;
+		diff = 0;
+		for (i = 1; i < n; i++) {
+			id = buf[i][idx[i]].frml_id;
+			if (id != max) {
+				if (id > max) 
+					max = id;
+				diff = 1;
 			}
 		}
 
-		/* find min value to fetch */
-		min = ~0;
-		for (i = 0; i < n; i++)
-			if (idx[i] != end[i])
-				if (buf[i][idx[i]].frml_id < min)
-					min = buf[i][idx[i]].frml_id;
+		printf("for (%d) and (%d): max=%d\n", 
+		       buf[0][idx[0]].frml_id, buf[1][idx[1]].frml_id, max);
+		
+		term_cond = 0;
+		if (diff) {
+			for (i = 0; i < n; i++) {
+				id = buf[i][idx[i]].frml_id;
+				if (id != max) {
+					printf("(%d) @ pipe %d ", 
+					       id, i, max);
+					jump(max, (struct brw_blk*)&buf[i], fh[i], 
+					     &idx[i], &end[i]);
+					if (idx[i] == end[i]) {
+						term_cond = 1;
+						break;
+					}
+				}
+			}
+		} else {
+			for (i = 0; i < n; i++) {
+				while (idx[i] < end[i]) {
+					id = buf[i][idx[i]].frml_id;
+					if (id != max)
+						break;
 
-		/* nothing to fetch for any pass, break */
-		if (min == ~0)
-			break;
-
-		/* skip all min values for each pass */
-		for (i = 0; i < n; i++) {
-			while (idx[i] < BRWBLK_RD_NUM && idx[i] < end[i]) {
-				if (buf[i][idx[i]].frml_id == min) {
-					//printf("%s: %d\n", fname[i], min);
-					check_sum += min;
+					printf("(%d) @ pipe %d calls\n", id, i);
 					idx[i] ++;
-				} else
-					break;
+					if (idx[i] == BRWBLK_RD_NUM) {
+						rebuf(&buf[i], fh[i], &idx[i], &end[i]);
+					}
+				}
+
+				if (idx[i] == end[i]) 
+					term_cond = 1;
 			}
 		}
-	} while (1);
+	} while (!term_cond);
 
 	free(end);
 	free(idx);
@@ -148,30 +185,23 @@ exit:
 
 	free(fh);
 
-	return check_sum;
+	return 0;
 }
 
 int
 main()
 {
-	int i;
-	unsigned int check_sum = 0;
-	char fname[][32] = {"posting-1.bin", 
-	                    "posting-2.bin", 
-	                    "posting-3.bin",
-	                    "posting-4.bin"
-						};
-	srand(time(NULL));
-	check_sum += write("posting-1.bin");
-	check_sum += write("posting-2.bin");
-	check_sum += write("posting-3.bin");
-	check_sum += write("posting-4.bin");
-	printf("check_sum = %d\n", check_sum);
+	char fname[][32] = {"posting-0.bin", "posting-1.bin"};
+	srand(123);
 
-	for (i = 0; i < 100; i++) {
-		check_sum = read2(fname, 4);
-	}
-	
-	printf("check_sum = %d\n", check_sum);
+	system("rm -f *.bin");
+	write("posting-0.bin");
+	write("posting-1.bin");
+
+	read("posting-0.bin");
+	read("posting-1.bin");
+
+	posmerge(fname, 2);
+
 	return 0;
 }
