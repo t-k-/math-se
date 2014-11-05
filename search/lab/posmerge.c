c410cff9d6d347f0587c5c9e344f4f2a40c2c982
t-k- <clock126@126.com>
2014-11-05 18:13:45 -0500

    better posmerge.c

--- search/lab/posmerge.c
+++ search/lab/posmerge.c
@@ -22,16 +22,59 @@ void write(const char *fname)
 		return;
 
 	blk.frml_id = 0;
-	delta = rand() % 10;
-	for (i = 0; i < 30 + delta; i++) {
-		blk.frml_id += rand() % 5;
+	delta = rand() % 15;
+	for (i = 0; i < 40 + delta; i++) {
+		blk.frml_id += rand() % 15;
 		fwrite(&blk, sizeof(struct brw_blk), 1, fh);
 	}
 	
 	fclose(fh);
 }
 
-#define BRWBLK_RD_NUM 8 // 4096
+#define SIZE sizeof(struct brw_blk)
+
+void write_case0(const char *fname)
+{
+	FILE *fh = fopen(fname, "a");
+	struct brw_blk blk;
+	if (fh == NULL)
+		return;
+	blk.frml_id = 1; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 2; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 3; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 4; fwrite(&blk, SIZE, 1, fh);
+
+	blk.frml_id = 5; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 6; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 7; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 8; fwrite(&blk, SIZE, 1, fh);
+	
+	blk.frml_id = 9; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 9; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 9; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 10; fwrite(&blk, SIZE, 1, fh);
+	
+	blk.frml_id = 12; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 15; fwrite(&blk, SIZE, 1, fh);
+	
+	fclose(fh);
+}
+
+void write_case1(const char *fname)
+{
+	FILE *fh = fopen(fname, "a");
+	struct brw_blk blk;
+	if (fh == NULL)
+		return;
+	blk.frml_id = 0; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 2; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 14; fwrite(&blk, SIZE, 1, fh);
+	blk.frml_id = 15; fwrite(&blk, SIZE, 1, fh);
+	
+	fclose(fh);
+}
+
+#define BRWBLK_RD_NUM 4 //4096
 #define BRWBLK_UNIT_SZ sizeof(struct brw_blk)
 #define BRWBLK_RD_BUF_SZ (BRWBLK_RD_NUM * BRWBLK_UNIT_SZ) 
 
@@ -63,47 +106,46 @@ int read(const char *fname)
 	return res;
 }
 
-static void 
+static unsigned int 
 rebuf(void *buf, FILE *fh,
       unsigned int *idx, unsigned int *end)
 {
-	unsigned int nread;
+	unsigned int nread, skips = *end - *idx;
 	nread = fread(buf, BRWBLK_UNIT_SZ, BRWBLK_RD_NUM, fh);
 	*idx = 0;
 	*end = nread;
-	printf("rebuf: read %d\n", nread);
+
+	return skips;
 }
 
-static void 
-jump(unsigned int target_id, struct brw_blk *buf, FILE *fh,
+static unsigned int
+jump(unsigned int target, struct brw_blk *buf, FILE *fh,
      unsigned int *idx, unsigned int *end)
 {
-	unsigned int nread, i, idx_far;
+	unsigned int idx_far, jumps = 0;
 	do {
 		idx_far = *end - 1;
-		if (buf[idx_far].frml_id >= target_id)
+		if (buf[idx_far].frml_id >= target)
 			break;
-		rebuf(buf, fh, idx, end);
-	} while (*idx < *end);
+		else if (*end != BRWBLK_RD_NUM)
+			return 0; 
+		
+		jumps += rebuf(buf, fh, idx, end);
+	} while (1);
 
-	if (*idx == *end)
-		return;
-
-	for (i = *idx; i < *end; i++) {
-		if (buf[i].frml_id >= target_id)
-			break;
+	while (*idx < *end && buf[*idx].frml_id < target) {
+		(*idx) ++;
+		jumps ++;
 	}
 
-	*idx = i; 
-	printf("jump to (%d)\n", buf[i].frml_id);
+	return jumps;
 }
 
 unsigned int posmerge(char (*fname)[32], int n)
 {
-	int i, term_cond;
 	FILE **fh = calloc(n, sizeof(FILE*));
 	struct brw_blk (*buf)[BRWBLK_RD_NUM];
-	unsigned int *idx, *end, max, id, diff;
+	unsigned int i, *idx, *end, max, id, diff, go_on, jumps = 0;
 
 	for (i = 0; i < n; i++) {
 		fh[i] = fopen(fname[i], "r");
@@ -126,52 +168,51 @@ unsigned int posmerge(char (*fname)[32], int n)
 	do { 
 		max = buf[0][idx[0]].frml_id;
 		diff = 0;
+		printf("%d: (%d) \n", 0, max);
 		for (i = 1; i < n; i++) {
 			id = buf[i][idx[i]].frml_id;
+			printf("%d: (%d) \n", i, id);
 			if (id != max) {
 				if (id > max) 
 					max = id;
 				diff = 1;
 			}
 		}
-
-		printf("for (%d) and (%d): max=%d\n", 
-		       buf[0][idx[0]].frml_id, buf[1][idx[1]].frml_id, max);
+		printf("\n");
 		
-		term_cond = 0;
+		go_on = 1;
 		if (diff) {
 			for (i = 0; i < n; i++) {
 				id = buf[i][idx[i]].frml_id;
 				if (id != max) {
-					printf("(%d) @ pipe %d ", 
-					       id, i, max);
-					jump(max, (struct brw_blk*)&buf[i], fh[i], 
-					     &idx[i], &end[i]);
-					if (idx[i] == end[i]) {
-						term_cond = 1;
+					go_on = jump(max, (struct brw_blk*)&buf[i], 
+				                 fh[i], &idx[i], &end[i]); 
+					if (!go_on)
 						break;
+					else if (go_on > 1) {
+						printf("jumped %d\n", go_on);
+						jumps += go_on;
 					}
 				}
 			}
 		} else {
 			for (i = 0; i < n; i++) {
-				while (idx[i] < end[i]) {
-					id = buf[i][idx[i]].frml_id;
-					if (id != max)
-						break;
-
-					printf("(%d) @ pipe %d calls\n", id, i);
+				while (buf[i][idx[i]].frml_id == max) {
+					printf("[%d] @ pipe %d calls\n", 
+					       buf[i][idx[i]].frml_id, i);
 					idx[i] ++;
-					if (idx[i] == BRWBLK_RD_NUM) {
-						rebuf(&buf[i], fh[i], &idx[i], &end[i]);
+					if (idx[i] == end[i]) {
+						if (end[i] == BRWBLK_RD_NUM) {
+							rebuf(&buf[i], fh[i], &idx[i], &end[i]);
+						} else {
+							go_on = 0;
+							break;
+						}
 					}
 				}
-
-				if (idx[i] == end[i]) 
-					term_cond = 1;
 			}
 		}
-	} while (!term_cond);
+	} while (go_on);
 
 	free(end);
 	free(idx);
@@ -185,6 +226,7 @@ exit:
 
 	free(fh);
 
+	printf("total jumps: %d\n", jumps);
 	return 0;
 }
 
@@ -192,16 +234,17 @@ int
 main()
 {
 	char fname[][32] = {"posting-0.bin", "posting-1.bin"};
-	srand(123);
+	srand(2132);
 
 	system("rm -f *.bin");
 	write("posting-0.bin");
 	write("posting-1.bin");
+//	write_case0("posting-0.bin");
+//	write_case1("posting-1.bin");
 
 	read("posting-0.bin");
 	read("posting-1.bin");
 
 	posmerge(fname, 2);
-
 	return 0;
 }
